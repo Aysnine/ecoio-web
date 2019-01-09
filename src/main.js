cabce94aa910bad758770960fcfe0370682bd30d
CNine <cnine229@gmail.com>
2019-01-09 18:34:43 +0800

    ready: add element-ui & normalize.css

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import router from './router'
 import store from './store'
 import './lib/main/pwa/registerServiceWorker'
 
+import ElementUI from 'element-ui'
+
+import 'normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.styl'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
