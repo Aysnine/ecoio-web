1c055064662f2b35fbe21f48995188a53ce392e6
CNine <cnine229@gmail.com>
2019-01-09 16:01:15 +0800

    ready: add src/lib/main & move registerServiceWorker.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
+import './lib/main/pwa/registerServiceWorker'
 
 Vue.config.productionTip = false
 
