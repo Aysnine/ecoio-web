4d4a77b20027c6e24d3ad305241592f801a6332c
CNine <cnine229@gmail.com>
2019-01-10 19:53:43 +0800

    page: add 404 page

---
+++ src/pages/404/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div(style='padding: 30px 0; text-align: center')
+    p(style='font-size: 4em; font-weight: bold; margin: 10px 0;') 404
+    p.desc 页面不存在
+    img(src='@/assets/fun/0.png', style='max-width: 360px; max-height: 360px;')
+    p.desc
+      router-link(to='/', style='margin: 1em')
+        el-button(type='primary', round, icon='el-icon-location-outline') 返回首页
+      el-button(type='primary', plain, round, icon='el-icon-arrow-left', @click='$router.go(-1)', style='margin: 1em') 上一页
+</template>
