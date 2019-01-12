7c503a4ccdb328d5ff681b5f4cb402db8d40af24
CNine <cnine229@gmail.com>
2019-01-12 16:59:56 +0800

    page: splash add footer

--- src/layout/splash/index.vue
+++ src/layout/splash/index.vue
@@ -5,9 +5,13 @@
         ghost-menu(:menu='menu')
       el-main.pm-0
         el-scrollbar(ref='main-scrollbar')
-          div(style='height: calc(100vh - 60px)')
+          div(style='height: calc(100vh - 120px)')
             transition(name='el-fade-in', mode='out-in')
               router-view
+      el-footer.ta-c(style='color: #ffffffbf')
+        span.fs-es= process.env.VUE_APP_FOOTER
+          a(href=process.env.VUE_APP_GH_REPO, target='_blanck')
+            el-button(type='text', style='color: #ff0')= ' @' + process.env.VUE_APP_GH_NAME
 </template>
 
 <script>
