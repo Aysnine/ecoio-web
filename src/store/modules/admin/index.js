8bd3a996964e103a712d4829ccdfd95a3cf56d6c
CNine <cnine229@gmail.com>
2019-02-27 14:05:19 +0800

    switch weather & account page

--- src/store/modules/admin/index.js
+++ src/store/modules/admin/index.js
@@ -7,8 +7,8 @@ export default {
     menu: [
       { label: '控制台', icon: 'dashboard', path: '/admin/dashboard' },
       { label: '设备管理', icon: 'device', path: '/admin/device' },
-      { label: '天气信息', icon: 'weather', path: '/admin/weather' },
       { label: '用户管理', icon: 'account', path: '/admin/account' },
+      { label: '天气信息', icon: 'weather', path: '/admin/weather' },
       { label: '服务器', icon: 'system', path: '/admin/system' }
     ]
   },
